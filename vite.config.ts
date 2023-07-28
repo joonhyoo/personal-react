1ca8aa494b89f2fb7c902feef3317bc44447f69b
Joon <joonhyoo12@gmail.com>
2023-07-28 16:36:05 +1000

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default ({
+export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/personal-react/'
 })
