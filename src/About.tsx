c73849f4496bde563d0cb7fa15faff58e3a5bfad
Joon <joonhyoo12@gmail.com>
2023-08-02 12:20:39 +1000

    styled page to work on mobile and web need to work on social buttons and hamburger menu

--- src/About.tsx
+++ src/About.tsx
@@ -1,18 +1,28 @@
 import "./about.css";
+import me1 from "./assets/me-1.jpeg";
 
 function About() {
   return (
     <div className="about-container">
       <h1 className="about-heading">About Me</h1>
       <div className="about-text-container">
-        <p>
-          I'm a 2nd year Computer Science student at the University of New South
-          Wales. I am passionate about web and mobile development.
-        </p>
-        <p>
-          I love to learn new things, and I try to stay updated on the newest
-          technologies.
-        </p>
+        <div className="about-text">
+          <p>
+            I'm a 2nd year Computer Science student at the University of New
+            South Wales. I am passionate about web and mobile development.
+          </p>
+          <p>
+            I love to learn new things, and I try to stay updated on the newest
+            technologies.
+          </p>
+          <p>
+            When I'm not developing, you'll probably see me playing volleyball
+            with friends, or cooking up a new dish!
+          </p>
+        </div>
+        <div className="about-image-container">
+          <img src={me1} className="about-image"></img>
+        </div>
       </div>
       <div className="about-heading"></div>
     </div>
