e7828b5a54c940a99e0dab95e84c4ecc36f8336f
Joon <joonhyoo12@gmail.com>
2023-08-29 15:27:57 +1000

    added gymshark repo to projects

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,7 +1,7 @@
 import Project from "../components/Project";
 import "../styles/projects.css";
 import reactSite from "../assets/React-Site.jpg";
-import catImage1 from "../assets/cat-1.jpeg";
+import gymDolphin from "../assets/gym-dolphin.png";
 import catImage2 from "../assets/cat-2.jpeg";
 import catImage3 from "../assets/cat-3.jpeg";
 
@@ -17,23 +17,23 @@ function Projects() {
           github={"https://github.com/joonhyoo/personal-react"}
         />
         <Project
-          title={"Project 2"}
-          image={catImage1}
-          description={"basic project"}
-          github={"some github repo"}
-        />{" "}
-        <Project
+          title={"Gym Dolphin"}
+          image={gymDolphin}
+          description={"Web App clone of mobile app GymShark"}
+          github={"https://github.com/joonhyoo/gymdolphin"}
+        />
+        {/* <Project
           title={"Project 3"}
           image={catImage2}
           description={"basic project"}
           github={"some github repo"}
-        />{" "}
+        />
         <Project
           title={"Project 4"}
           image={catImage3}
           description={"basic project"}
           github={"some github repo"}
-        />
+        /> */}
       </div>
     </div>
   );
